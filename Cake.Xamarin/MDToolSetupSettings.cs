13ace9f8c3455f6e5aaf1b57f39eac2793f71627
Alistair Chapman <alistairgchapman@gmail.com>
2016-12-04 02:08:50 +1000

    Add initial support for the mdtool setup utility - Currently only supports packaging commands

---
+++ Cake.Xamarin/MDToolSetupSettings.cs
@@ -0,0 +1,12 @@
+﻿using Cake.Core.Tooling;
+
+namespace Cake.Xamarin
+{
+    /// <summary>
+    /// Settings for the <see cref="MDToolSetupRunner"/>.
+    /// </summary>
+    public class MDToolSetupSettings : ToolSettings
+    {
+
+    }
+}
